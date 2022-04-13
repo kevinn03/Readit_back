89b225c72492551c922be0f4a23c5d55e2f712e1
Kevin <kevin03.nguyen@gmail.com>
2022-04-13 14:09:40 -0400

    add static build

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import redditRouter from './routes/reddit';
 import cors from 'cors';
 import middleware from './utils/middleware';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/.env' });
 const app = express();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors());
-const PORT = 3001;
+app.use(express.static('build'));
+const PORT = process.env.PORT || 3001;
 
 app.use('/api/reddit', redditRouter);
 app.use(middleware.unknownEndpoint);
